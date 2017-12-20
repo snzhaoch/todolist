8a1ff3aa2113547296571b13d3acb48d2ce9f9eb
snzhaoch <snzhaoch@outlook.com>
2017-12-20 22:39:19 +0800

    更改 host 与 port

--- server.py
+++ server.py
@@ -51,7 +51,7 @@ def run(host, port):
 if __name__ == '__main__':
     # 生成配置并且运行程序
     config = dict(
-        host='127.0.0.1',
-        port=8800,
+        host='0.0.0.0',
+        port=80,
     )
     run(**config)
